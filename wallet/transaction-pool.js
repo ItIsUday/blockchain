28b1f6da032733ac1a00792f480fc940ba5db337
Uday <iamuday1618@gmail.com>
2023-01-05 23:08:46 +0530

    Create Transaction Pool and write its tests

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,23 @@
+class TransactionPool {
+  constructor() {
+    this.transactions = [];
+  }
+
+  updateOrAddTransaction(transaction) {
+    const transactionIndex = this.transactions.findIndex(
+      (t) => t.id === transaction.id
+    );
+
+    if (transactionIndex >= 0) {
+      this.transactions[transactionIndex] = transaction;
+    } else {
+      this.transactions.push(transaction);
+    }
+  }
+
+  existingTransaction(address) {
+    return this.transactions.find((t) => t.input.address === address);
+  }
+}
+
+module.exports = TransactionPool;
