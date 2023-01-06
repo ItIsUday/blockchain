9355ccc19bf2603455cc7dd2a707c47644d4770c
Uday <iamuday1618@gmail.com>
2023-01-06 11:51:09 +0530

    Add support for broadcasting transactions

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const Blockchain = require("../blockchain");
 const bodyParser = require("body-parser");
 const P2pServer = require("./p2p-server");
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/transaction-pool");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
